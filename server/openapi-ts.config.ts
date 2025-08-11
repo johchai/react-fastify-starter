63efd3af8efaa17a74d83de2382a39b87933a4d1
Johnny Chai <hello@johnnychai.com>
2025-08-11 15:47:33 +0800

    refactor: update openapi-ts path

--- server/openapi-ts.config.ts
+++ server/openapi-ts.config.ts
@@ -1,5 +1,4 @@
 // config for @hey-api/openapi-ts to generate TypeScript types and SDK from OpenAPI spec
-
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
@@ -7,32 +6,31 @@ export default defineConfig({
   output: "../packages/types/src/openapi",
   parser: {
     patch: {
-      version: "3.1.1", // TEMP FIX: https://github.com/hey-api/openapi-ts/issues/2169
-    },
+      version: "3.1.1" // TEMP FIX: https://github.com/hey-api/openapi-ts/issues/2169
+    }
   },
   plugins: [
     {
       name: "@hey-api/client-fetch",
-      exportFromIndex: true,
+      exportFromIndex: true
     },
     {
       name: "@hey-api/sdk",
       auth: true,
       validator: true,
-      exportFromIndex: true,
+      exportFromIndex: true
     },
     {
       name: "@tanstack/react-query",
-      exportFromIndex: true,
+      exportFromIndex: true
     },
-
     {
       name: "zod",
       requests: true,
       definitions: true,
       compatibilityVersion: 3,
       metadata: true,
-      exportFromIndex: true,
-    },
-  ],
+      exportFromIndex: true
+    }
+  ]
 });
